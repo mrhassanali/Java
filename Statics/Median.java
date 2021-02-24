f707046f6e7890e13bbf2e46abf9397da282fa01
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-02-24 13:46:28 +0500

    Update Median.java

--- Statics/Median.java
+++ Statics/Median.java
@@ -2,28 +2,28 @@ import java.util.*;
 class Sort{
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
-		int i,j,temp;
+		int w,z,temp;
 		System.out.println("Enter Total Value");
 		int n=input.nextInt();
 
 		int[] x = new int[n];
 		System.out.println("Enter "+ n+" Value");
-		for(i=0;i<n;i++){
+		for(w=0;w<n;w++){
 				int a=input.nextInt();
-				x[i]=a;
+				x[w]=a;
 			}
-			for(i=0;i<n; i++){
-				for(j=0;j<n-1; j++){
-					if(x[j]>x[j+1]){
-						temp=x[j];
-						x[j]=x[j+1];
-						x[j+1]=temp;
+			for(w=0;w<n; w++){
+				for(z=0;z<n-1; z++){
+					if(x[z]>x[z+1]){
+						temp=x[z];
+						x[z]=x[z+1];
+						x[z+1]=temp;
 						}
 					}
 			}
 			System.out.println("Array Sorting");
-			for(i=0;i<n; i++){
-				System.out.print(x[i]+",");
+			for(w=0;w<n; w++){
+				System.out.print(x[w]+",");
 				}
 				/*Line Changing*/
 				System.out.println("\n");
@@ -34,18 +34,14 @@ class Sort{
 					int p=(n/2)-1;
 					int r;
 					r = p+1;
-					System.out.println("x[p] ="+x[p]);
-					System.out.println("x[r] ="+x[r]);
 					double mm=(x[p]+x[r])/2;
-					System.out.println("x[p]+x[p] ="+(x[p]+x[r]));
-					System.out.println("x[p] ="+mm);
 					double dd=(x[p]+x[r]);
 					double dr = dd/2;
-				    System.out.println(dr);
+				    System.out.println("Median = "+dr);
 					}
 					else{ // For Odd
-					for(i=0;i<n; i++){
-						System.out.println((i+1)+"="+x[i]);
+					for(w=0;w<n; w++){
+						System.out.println((w+1)+"="+x[w]);
 				      }
 					int odd=(n+1)/2;
 					System.out.println("Median is Above "+odd+" Value");
