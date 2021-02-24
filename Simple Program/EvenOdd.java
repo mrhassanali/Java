1aa84d9d4c876524ac801f411cc97a74737366e5
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-02-24 12:25:18 +0500

    Create EvenOdd.java
    In this Program, you can be able to find the input number is Even or Odd

---
+++ Simple Program/EvenOdd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class EvenOdd{
+	public static void main(String [] args){
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		if(n%2 == 0){
+			System.out.println("Even");
+			}
+			else{
+				System.out.println("odd");
+				}
+		}
+	}
