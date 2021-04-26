05a1672598130e11604fff370f7ffc30938bf585
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-04-26 13:15:03 +0500

    Add files via upload

---
+++ Simple Program/RecursiveMethod.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class RecursiveMethod{
+		public static void main(String[]args)
+		 {
+			System.out.println("Hello");
+		    pakistan();
+		 }
+			public static void pakistan()
+			 {
+			   System.out.println("World!");
+			 }
+}
