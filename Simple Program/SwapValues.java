56ac03e7a64d99d3d2400b36cc9d2ab6bc74990d
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-04-26 12:59:55 +0500

    Add files via upload

---
+++ Simple Program/SwapValues.java
@@ -0,0 +1,14 @@
+class SwapValues{
+	public static void main(String [] args){
+		int a,b;
+		a = 10 ;
+		b = 20 ;
+
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("a ="+a);
+		System.out.println("b ="+b);
+		}
+	}
